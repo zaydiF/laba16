f9660f5e276cb63153114ae81d3c7c02888a1f5a
zaydiF <dimonkit2007@mail.ru>
2025-05-08 21:33:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("code.txt");
+        try(FileWriter fw = new FileWriter("code.txt"); BufferedReader br = new BufferedReader(new FileReader("codncom.txt"))){
+            String line = br.readLine();
+            while (line != null) {
+                //System.out.println(line);
+                if ((line.contains("//") && (line.charAt(0) != '/')) && (!line.contains("/\""))){
+                    fw.write(line.substring(0,line.indexOf("//")) + "\n");
+                    System.out.println(line.substring(0,line.indexOf("//")) + "\n");
+                }
+                else if ((line.contains("//") && (line.charAt(0) == '/'))) {
+                }
+                else if(line.contains("/*") && (!line.contains("/*\""))){
+                    fw.write(line.substring(0,line.indexOf("/*"))+"\n");
+                    System.out.println(line.substring(0,line.indexOf("/*")));
+                    while(!line.contains("*/")){
+                        line=br.readLine();
+                    }
+                }
+                else {
+                    fw.write(line + "\n");
+                    System.out.println(line);
+                }
+                line = br.readLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (NullPointerException ex){
+
+        }
+    }
+}
